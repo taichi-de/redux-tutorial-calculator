3d6646c9bb80779ad60c00c028dead25774d71f2
taichi-de <61309248+taichi-de@users.noreply.github.com>
2021-03-01 14:43:08 +0100

    finish UI & add redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.scss";
+import Result from "./components/Result";
+import Button from "./components/Button";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <div className="result">
+        <Result result={"0"} />
+      </div>
+      <div className="button-wrapper">
+        <div className="number">
+          <div className="upper">
+            <Button text={"7"} />
+            <Button text={"8"} />
+            <Button text={"9"} />
+          </div>
+          <div className="middle">
+            <Button text={"4"} />
+            <Button text={"5"} />
+            <Button text={"6"} />
+          </div>
+          <div className="lower">
+            <Button text={"1"} />
+            <Button text={"2"} />
+            <Button text={"3"} />
+          </div>
+          <div className="bottom">
+            <Button text={"0"} />
+            <Button text={"AC"} />
+            <Button text={"="} />
+          </div>
+        </div>
+        <div className="operator">
+          <Button text={"÷"} />
+          <Button text={"×"} />
+          <Button text={"-"} />
+          <Button text={"+"} />
+        </div>
+      </div>
+    </React.Fragment>
   );
-}
+};
 
 export default App;
---
+++ src/components/Result.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Result = ({ result }) => {
+  return <div className="resultValue">{result}</div>;
+};
+
+export default Result;
